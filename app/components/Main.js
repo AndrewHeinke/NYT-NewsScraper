c94b91e0a0f968fb69663ba4a98e2e635779f96e
Andrew Heinke <andrew.j.heinke@gmail.com>
2016-09-09 21:51:41 -0500

    setting up react router routes

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,35 +1,10 @@
-// Include React
 var React = require('react');
+var ReactDOM = require('react-dom');
+var Router = require('react-router').Router;
 
-var Main = React.createClass({
+var routes = require('./config/routes');
 
-	// Here we render the function
-	render: function(){
-
-		return(
-
-			<div className="container">
-				<div className="jumbotron">
-					<h2><strong>Which Child???</strong></h2>
-					<p><em>A journey through the whimsical world of React Routing</em></p>
-					<hr/>
-					<p>
-						<a href="#/Child1"><button className="btn btn-primary btn-lg">Show Child #1</button></a>
-						<a href="#/Child2"><button className="btn btn-danger btn-lg">Show Child #2</button></a>
-					</p>
-				</div>
-
-				<div className="row">
-
-					{/*This code will dump the correct Child Component*/}
-					{this.props.children}
-
-				</div>
-
-			</div>
-		)
-	}
-});
-
-// Export the componen back for use in other files
-module.exports = Main;
+ReactDOM.render(
+	<Router>{routes}</Router>,
+	document.getElementById('app');
+)
